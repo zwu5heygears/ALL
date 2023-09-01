6384cc45d4ac0f142f29eb04609a23c624f16a5c
zwu5heygears <zwu5@heygears.com>
2023-09-01 16:20:24 +0800

    change

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
+#include <thread>
+//#include <onnx_runtime>
+//#include "PID.h"
+#include <chrono>
+using namespace cv;
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Mat src = imread("/home/heygears/CLionProject/ALL/1.png", IMREAD_REDUCED_COLOR_4);
+    if(src.empty()){
+        cout << "src is empty" << endl;
+        return 0;
+    }
+    imshow("1", src);
+    waitKey();
     return 0;
 }
