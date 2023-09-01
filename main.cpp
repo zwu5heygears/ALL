bf0c8915f5bc7718984fae2ce605bdf1dd992057
zwu5heygears <zwu5@heygears.com>
2023-09-01 16:36:39 +0800

    add chrono boost

--- main.cpp
+++ main.cpp
@@ -4,15 +4,35 @@
 //#include <onnx_runtime>
 //#include "PID.h"
 #include <chrono>
+#include <sstream>
+#include <boost/asio.hpp>
+#include <boost/asio/thread_pool.hpp>
+#include <boost/thread.hpp>
+
 using namespace cv;
 using namespace std;
-int main() {
+
+void ShowMat(){
     Mat src = imread("/home/heygears/CLionProject/ALL/1.png", IMREAD_REDUCED_COLOR_4);
     if(src.empty()){
         cout << "src is empty" << endl;
-        return 0;
     }
-    imshow("1", src);
+    imshow("0", src);
     waitKey();
+}
+int main() {
+    boost::asio::thread_pool mPool(2);
+    boost::asio::post(mPool, ShowMat);
+    boost::asio::post(mPool, ShowMat);
+    ShowMat();
+
+    auto time = chrono::system_clock::now();
+    auto time_t = chrono::system_clock::to_time_t(time);
+    auto tm_now = localtime(&time_t);
+    cout << put_time(tm_now, "%Y-%m-%d %H:%M:%S") << endl;
+    stringstream ss;
+    ss << put_time(tm_now, "%Y-%m-%d %H:%M:%S");
+    string timestr = ss.str();
+    cout << timestr << endl;
     return 0;
 }
